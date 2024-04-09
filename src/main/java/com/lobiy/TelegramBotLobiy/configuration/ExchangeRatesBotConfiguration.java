11d1441d710affa606088cbb96fe4c46784e0827
Yuriy Lobanov <yuriy.lobanov2002@gmail.com>
2024-04-09 19:49:38 +0200

    added caching

--- src/main/java/com/lobiy/TelegramBotLobiy/configuration/ExchangeRatesBotConfiguration.java
+++ src/main/java/com/lobiy/TelegramBotLobiy/configuration/ExchangeRatesBotConfiguration.java
@@ -2,13 +2,17 @@ package com.lobiy.TelegramBotLobiy.configuration;
 
 import com.lobiy.TelegramBotLobiy.bot.ExchangeRatesBot;
 import okhttp3.OkHttpClient;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
+@EnableCaching
+@EnableScheduling
 public class ExchangeRatesBotConfiguration {
 
     @Bean
--- src/main/java/com/lobiy/TelegramBotLobiy/service/impl/ExchangeRatesServiceImpl.java
+++ src/main/java/com/lobiy/TelegramBotLobiy/service/impl/ExchangeRatesServiceImpl.java
@@ -3,7 +3,10 @@ package com.lobiy.TelegramBotLobiy.service.impl;
 import com.lobiy.TelegramBotLobiy.Exception.ServiceException;
 import com.lobiy.TelegramBotLobiy.client.CbrClient;
 import com.lobiy.TelegramBotLobiy.service.ExchangeRatesService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.xml.sax.InputSource;
 
@@ -13,25 +16,38 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.StringReader;
-
+import org.slf4j.Logger;
 @Service
 public class ExchangeRatesServiceImpl implements ExchangeRatesService {
     @Autowired
     private CbrClient client;
 
+    private static final Logger LOG = LoggerFactory.getLogger(ExchangeRatesServiceImpl.class);
     private static final String USD_XPATH = "/ValCurs//Valute[@ID='R01235']/Value";
     private static final String EUR_XPATH = "/ValCurs//Valute[@ID='R01239']/Value";
 
+    @Cacheable(value = "usd", unless = "#result == null or #result.isEmpty()")
     @Override
     public String getUSDExchangeRate() throws ServiceException {
         var xml = client.getCurrencyRatesXML();
         return extractCurrencyValueFromXml(xml, USD_XPATH);
     }
 
+    @CacheEvict("usd")
+    public void clearUsdCache() {
+        LOG.info("Clearing USD cache");
+    }
+
+    @Cacheable(value = "eur", unless = "#result == null or #result.isEmpty()")
     @Override
     public String getEURExchangeRate() throws ServiceException {
         var xml = client.getCurrencyRatesXML();
         return extractCurrencyValueFromXml(xml, EUR_XPATH);
+    }
+
+    @CacheEvict("eur")
+    public void clearEurCache() {
+        LOG.info("Clearing EUR cache");
     }
 
     private static String extractCurrencyValueFromXml(String xml, String xpathExpression) throws ServiceException {
